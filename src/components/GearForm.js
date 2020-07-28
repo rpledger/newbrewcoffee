f819212696ea5dc77c69a316c420a0cdb0fc8ae5
Rebecca Pledger <rebeccapledger14@gmail.com>
2020-07-27 22:05:08 -0500

    Fix form handling and add more forms

--- src/components/GearForm.js
+++ src/components/GearForm.js
@@ -19,15 +19,13 @@ class GearForm extends Component {
     constructor(props){
         super(props);
         this.state = {
-            scaleType: "None",
-            kettleType: "None"
+            scaleType: "",
+            kettleType: ""
         };
-        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(event) {
-        console.log(event.target.name)
-        this.setState({[event.target.name]: event.target.value});
+    handleChange(e) {
+        this.setState({ [e.target.name] : e.target.value });
     }
 
     render() {
@@ -38,29 +36,67 @@ class GearForm extends Component {
                     <InputLabel id="demo-simple-select-label">Scale?</InputLabel>
                     <Select
                         labelId="demo-simple-select-label"
+                        name="scaleType"
                         id="demo-simple-select"
                         value={this.state.scaleType}
-                        onChange={this.handleChange}
+                        onChange={this.handleChange.bind(this)}
                     >
-                        <MenuItem value={"None"}>No</MenuItem>
+                        <MenuItem value={"None"}>None</MenuItem>
                         <MenuItem value={"Kitchen Scale"}>Kitchen Scale</MenuItem>
                         <MenuItem value={"Tenth of a Gram Scale"}>Tenth of a Gram Scale</MenuItem>
                     </Select>
                 </FormControl>
+
                 <FormControl className={classes.formControl}>
                     <InputLabel id="demo-simple-select-label">Kettle?</InputLabel>
                     <Select
                         labelId="demo-simple-select-label"
+                        name="kettleType"
                         id="demo-simple-select"
                         value={this.state.kettleType}
-                        onChange={this.handleChange}
+                        onChange={this.handleChange.bind(this)}
                     >
-                        <MenuItem value={"None"}>No</MenuItem>
+                        <MenuItem value={"None"}>None</MenuItem>
                         <MenuItem value={"Stovetop Kettle"}>Stovetop Kettle</MenuItem>
                         <MenuItem value={"Electric Kettle"}>Electric Kettle</MenuItem>
-                        <MenuItem value={"Variable Temperature Electric Kettle"}>Variable Temperature Electric Kettle</MenuItem>
-                        <MenuItem value={"Gooseneck Stovetop Kettle"}>Gooseneck Stovetop Kettle</MenuItem>
-                        <MenuItem value={"Electric Gooseneck Kettle"}>Electric Gooseneck Kettle</MenuItem>
+                        <MenuItem value={"Variable Temperature Kettle"}>Variable Temperature Kettle</MenuItem>
+                        <MenuItem value={"Stovetop Gooseneck Kettle"}>Stovetop Gooseneck Kettle</MenuItem>
+                        <MenuItem value={"Electric Kettle"}>Electric Gooseneck Kettle</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="demo-simple-select-label">Grinder?</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        name="grinderType"
+                        id="demo-simple-select"
+                        value={this.state.Type}
+                        onChange={this.handleChange.bind(this)}
+                    >
+                        <MenuItem value={"None"}>None</MenuItem>
+                        <MenuItem value={"Blade Grinder"}>Blade Grinder</MenuItem>
+                        <MenuItem value={"Hand Burr Grinder"}>Hand Burr Grinder</MenuItem>
+                        <MenuItem value={"Electric Burr Grinder <120"}>Electric Burr Grinder under $120</MenuItem>
+                        <MenuItem value={"Electric Burr Grinder >120"}>Electric Burr Grinder over $120</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="demo-simple-select-label">Brewer?</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        name="brewerType"
+                        id="demo-simple-select"
+                        value={this.state.Type}
+                        onChange={this.handleChange.bind(this)}
+                    >
+                        <MenuItem value={"None"}>None</MenuItem>
+                        <MenuItem value={"French Press"}>French Press</MenuItem>
+                        <MenuItem value={"Aeropress"}>Aeropress</MenuItem>
+                        <MenuItem value={"Chemex"}>Chemex</MenuItem>
+                        <MenuItem value={"Kalita"}>Kalita</MenuItem>
+                        <MenuItem value={"Hario v60"}>Hario v60</MenuItem>
                     </Select>
                 </FormControl>
             </div>
